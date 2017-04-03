2c2fa0d5bb75621a3c95fc36c260107929896f5d
James Kominick <james.kominick@gmail.com>
2017-04-03 19:52:08 -0400

    Update crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
 /*!
-A macro for defining functions that wrap a static-ref cache object.
+A macro for defining cached/memoized functions that wrap a static-ref cache object.
 
-# Options:
+# Usage & Options:
 
-1.) Use the default unbounded cache
+There's several option depending on how explicit you want to be. See below for full syntax breakdown.
+
+1.) Use an explicitly specified cache-type and provide the instantiated cache struct.
+    For example, a `SizedCache` (LRU).
 
 
-```rust,ignore
-cached!{CACHE_NAME >>
-func_name(arg1: arg1_type, arg2: arg2_type) -> return_type = {
-    <regular function body>
+```rust
+#[macro_use] extern crate cached;
+#[macro_use] extern crate lazy_static;
+
+use cached::{Cached, SizedCache};
+
+cached!{FIB: SizedCache = SizedCache::with_capacity(50); >>
+fib(n: u64) -> u64 = {
+    if n == 0 || n == 1 { return n }
+    fib(n-1) + fib(n-2)
 }}
+
+pub fn main() { }
 ```
 
 
@@ -18,28 +29,77 @@ func_name(arg1: arg1_type, arg2: arg2_type) -> return_type = {
     The cache-type is expected to have a `new` method that takes no arguments.
 
 
-```rust,ignore
-cached!{CACHE_NAME: SpecificCacheType >>
-func_name(arg1: arg1_type, arg2: arg2_type) -> return_type = {
-    <regular function body>
+```rust
+#[macro_use] extern crate cached;
+#[macro_use] extern crate lazy_static;
+
+use cached::{Cached, UnboundCache};
+
+cached!{FIB: UnboundCache >>
+fib(n: u64) -> u64 = {
+    if n == 0 || n == 1 { return n }
+    fib(n-1) + fib(n-2)
 }}
+
+pub fn main() { }
 ```
 
 
-3.) Use an explicitly specified cache-type and provide the instantiated cache struct.
-    This allows using caches that require args in their constructor or have a constructor
-    method other than a simple `new`.
+3.) Use the default unbounded cache.
 
 
-```rust,ignore
-cached!{CACHE_NAME: MyCache = MyCache::with_capacity(arg); >>
-func_name(arg1: arg1_type, arg2: arg2_type) -> return_type = {
-    <regular function body>
+```rust
+#[macro_use] extern crate cached;
+#[macro_use] extern crate lazy_static;
+
+use cached::{Cached};
+
+cached!{FIB >>
+fib(n: u64) -> u64 = {
+    if n == 0 || n == 1 { return n }
+    fib(n-1) + fib(n-2)
 }}
+
+pub fn main() { }
 ```
 
 
-Custom cache types must implement `cached::Cached`
+# Cache Types
+
+Several caches are available in this crate:
+
+- `cached::UnboundCache`
+- `cached::SizedCache`
+- `cached::TimedCache`
+
+Any custom cache that implements `cached::Cached` can be used in place of the built-ins.
+
+# Syntax:
+
+The complete macro syntax is:
+
+
+```rust,ignore
+cached!{CACHE_NAME: CacheType = CacheType::constructor(arg); >>
+func_name(arg1: arg_type, arg2: arg_type) -> return_type = {
+    // do stuff like normal
+    return_type
+}}
+```
+
+Where:
+
+- `CACHE_NAME` is the unique name used to hold a `static ref` to the cache
+- `CacheType` is the struct type to use for the cache (Note, this cannot be namespaced, e.g.
+  `cached::SizedCache` will not be accepted by the macro. `SizedCache` must be imported and passed
+   directly)
+- `CacheType::constructor(arg)` is any expression that yields an instance of `CacheType` to be used
+  as the cache-store, followed by `; >>`
+- `func_name(arg1: arg_type) -> return_type` is the same form as a regular function signature, with the exception
+  that functions with no return value must be explicitly stated (e.g. `fn(arg: arg_type) -> ()`)
+- The expression following `=` is the function body assigned to `func_name`. Note, the function
+  body can make recursive calls to its cached-self (`func_name`).
+
 
 */
 
