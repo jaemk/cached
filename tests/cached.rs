638e1b05172029f972a3f1cfc5020e735cb03dfb
James Kominick <james.kominick@gmail.com>
2018-02-18 22:04:10 -0500

    update tests

--- tests/cached.rs
+++ tests/cached.rs
@@ -48,28 +48,47 @@ fn test_sized_cache() {
 
 
 cached!{
-    TIMED_FIB: TimedCache<(u32), u32> = TimedCache::with_lifespan_and_capacity(2, 5);
-    fn fib2(n: u32) -> u32 = {
-        if n == 0 || n == 1 { return n }
-        fib2(n-1) + fib2(n-2)
+    TIMED: TimedCache<(u32), u32> = TimedCache::with_lifespan_and_capacity(2, 5);
+    fn timed(n: u32) -> u32 = {
+        sleep(Duration::new(3, 0));
+        n
     }
 }
 
 
 #[test]
 fn test_timed_cache() {
-    fib2(1);
-    fib2(1);
+    timed(1);
+    timed(1);
     {
-        let cache = TIMED_FIB.lock().unwrap();
+        let cache = TIMED.lock().unwrap();
         assert_eq!(1, cache.cache_misses().unwrap());
         assert_eq!(1, cache.cache_hits().unwrap());
     }
-    sleep(Duration::new(2, 0));
-    fib2(1);
+    sleep(Duration::new(3, 0));
+    timed(1);
     {
-        let cache = TIMED_FIB.lock().unwrap();
+        let cache = TIMED.lock().unwrap();
         assert_eq!(2, cache.cache_misses().unwrap());
         assert_eq!(1, cache.cache_hits().unwrap());
     }
 }
+
+
+cached!{
+    STRING_CACHE_EXPLICIT: SizedCache<(String, String), String> = SizedCache::with_capacity(1);
+    fn string_1(a: String, b: String) -> String = {
+        return a + &b;
+    }
+}
+
+
+#[test]
+fn test_string_cache() {
+    string_1("a".into(), "b".into());
+    {
+        let cache = STRING_CACHE_EXPLICIT.lock().unwrap();
+        assert_eq!(1, cache.cache_size());
+    }
+}
+
